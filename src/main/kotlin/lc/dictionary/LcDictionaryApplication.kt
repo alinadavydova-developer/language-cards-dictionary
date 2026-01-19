b996dc85e67913bacfea9fa03ce117f6cdd6fb91
alina-gasprom <alina.k.91@icloud.com>
2026-01-19 21:40:34 +0300

    init

---
+++ src/main/kotlin/lc/dictionary/LcDictionaryApplication.kt
@@ -0,0 +1,11 @@
+package lc.dictionary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LcDictionaryApplication
+
+fun main(args: Array<String>) {
+    runApplication<LcDictionaryApplication>(*args)
+}
---
+++ src/test/kotlin/lc/dictionary/LcDictionaryApplicationTests.kt
@@ -0,0 +1,13 @@
+package lc.dictionary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LcDictionaryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
